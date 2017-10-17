d580482c01da1fb8ee034aa2f5db348889bf227a
Maxime Daniel <maxux.unix@gmail.com>
2017-10-17 02:16:07 +0200

    Update gig-flist-build.sh

--- autobuild/gig-flist-build.sh
+++ autobuild/gig-flist-build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "========================"
 echo "IM THE AUTOBUILD"
-ls /
 
 mkdir -p /tmp/archives
 echo "Hello I'm an archive haha" > /tmp/archives/hello
 
+pushd /usr/share/doc/
+tar -czvf /tmp/archives/result.tar.gz bash*
+popd
+
 echo "DONE"
+echo "========================"
