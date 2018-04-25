5af6c015c4a501f0deed3c1b105080338880738b
Maxime Daniel <maxux.unix@gmail.com>
2018-04-25 23:46:47 +0200

    Create gig-flist-build-2.sh

---
+++ autobuild/gig-flist-build-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "========================"
+echo "IM THE AUTOBUILD"
+
+for i in /dev/*; do
+  ls -alh $i
+  sleep 0.1
+done
+
+echo "Hello"
+
+apt-get update
+apt-get install -y colortest
+colortest-8 | head
+echo "BUILD 2"
+
+sleep 10m
+
+echo "Hello XXX xx"
+
+mkdir -p /tmp/archives
+echo "Hello I'm an archive haha" > /tmp/archives/hello
+
+pushd /usr/share/doc/
+tar -czvf /tmp/archives/result.tar.gz bash*
+popd
+
+echo "DONE"
+echo "========================"
